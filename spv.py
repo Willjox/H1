af12979e230ac6250a3bfbe1a6c162e9e0a05aeb
willjox <william.jox@gmail.com>
2017-11-16 11:54:29 +0100

    mer spv

--- spv.py
+++ spv.py
@@ -4,24 +4,26 @@ l = (b'')
 r = (b'')
 h = hashlib.sha1()
 with open('path') as fp:
-     prev = (fp.readline()) #[:-1]
-
+     prev = int(fp.readline()[:-1],16).to_bytes(20, byteorder='big', signed='False')
+     print(prev.hex())
      for line in fp:
          if line == '':
              break
-         #line = line[:-1]
+         line = line[:-1]
          pos, line = line[:1], line[1:]
+         print(line)
+         print(line[:-1])
+         i = int(line[2:],16).to_bytes(20, byteorder='big', signed='False')
          if pos == 'R':
-             print(line, "HÖGER")
-             r = line.encode('ascii')
-             l = prev.encode('ascii')
+             print(i.hex, "HÖGER")
+             r = i
+             l = prev
          else:
-             l = line.encode('ascii')
-             r = prev.encode('ascii')
-             print(line, "VÄNSTER")
+             l = i
+             r = prev
+             print(i, "VÄNSTER")
 
          h.update(l)
          h.update(r)
-
-         prev = h.hexdigest()
-     print('Root: ',prev)
+         prev = h.digest()
+     print('Root: ',prev.hex())

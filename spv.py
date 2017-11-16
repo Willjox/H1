d6736648846be527c01d7197aaf4e856c908cf79
willjox <william.jox@gmail.com>
2017-11-16 10:40:13 +0100

    spv

--- spv.py
+++ spv.py
@@ -1,8 +1,27 @@
+import hashlib
 
-
-
+l = (b'')
+r = (b'')
+h = hashlib.sha1()
 with open('path') as fp:
-    print(fp.readline())
-    print(fp.readline())
-    for line in fp:
-        print(line)
+     prev = (fp.readline()) #[:-1]
+
+     for line in fp:
+         if line == '':
+             break
+         #line = line[:-1]
+         pos, line = line[:1], line[1:]
+         if pos == 'R':
+             print(line, "HÖGER")
+             r = line.encode('ascii')
+             l = prev.encode('ascii')
+         else:
+             l = line.encode('ascii')
+             r = prev.encode('ascii')
+             print(line, "VÄNSTER")
+
+         h.update(l)
+         h.update(r)
+
+         prev = h.hexdigest()
+     print('Root: ',prev)

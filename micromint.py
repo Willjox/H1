e5e87e0bff32643f2b792e43c30901dda7492cc7
willjox <william.jox@gmail.com>
2017-11-21 17:37:45 +0100

    micromint

--- micromint.py
+++ micromint.py
@@ -1,9 +1,14 @@
 import hashlib
 import random
 import sys
-#u =int(input("u = "))
-#k =int(input("k = "))
-#c =int(input("c = "))
+import math
+
+
+def stddev(results, average):
+    y= []
+    for x in results:
+        y.append((average - x)**2)
+    return math.sqrt( ( (sum(y))/(len(results)-1) ))
 
 def mint(u,k,c):
     bu = int(u/8)
@@ -14,21 +19,35 @@ def mint(u,k,c):
     rand = random.SystemRandom()
 
     while i < c:
-        x = rand.randint(0, 2**31)
+        x = rand.randint(0, 2**26)
         h.update(bytes(x))
         boll = h.digest()[0:bu]
         iboll = int.from_bytes(boll,byteorder = 'big' ,signed =False)
         korg[iboll] = korg[iboll] + 1
-        #print("Boll: " , iboll)
-        #print("Bollar i korg:  " , korg[iboll])
+        print("Boll: " , iboll)
+        print("Bollar i korg:  " , korg[iboll])
         if  korg[iboll] == k:
             i = i + 1
-            #print("antal mynt: " ,i)
+            print("antal mynt: " ,i)
         p = p+1
-        #print(p)
+        print(p)
     return p
 
 k = 0
-while k < 10:
-    print(mint(16,2,1))
-    k = k+1
+avg = 0
+ci = 0
+s = 0
+u =int(input("u = "))
+k =int(input("k = "))
+c =int(input("c = "))
+z =int(input("Confidence = "))
+
+results = []
+while z > ci:
+    results.append((mint(u,k,c)))
+    avg = sum(results)/len(results)
+    if len(results) > 1:
+        s = stddev(results,avg)
+    ci = s/(math.sqrt(len(results)))
+    print(avg)
+    print(ci)

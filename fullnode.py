332b664aab17b13dbafebf56fe77301bfa51ed2e
willjox <william.jox@gmail.com>
2017-11-21 14:27:29 +0100

    asfsaf

--- fullnode.py
+++ fullnode.py
@@ -17,30 +17,43 @@ def climbTree(base):
     while(len(base) != 0):
         left = base.pop(0)
         right = base.pop(0)
-        parent = node(left.getHash(),right.getHash())
+        parent = node(left.getHash(),right.getHash(),'0x0')
+        left.adopt(parent)
+        right.adopt(parent)
         parents.append(parent)
 
     return parents
 def climbTreeRoot(base):
     parents = []
     while(len(base) != 0):
-        left = base.pop(0)
-        right = base.pop(0)
-        parent = node(left,right)
+        left = node('0x0','0x0',base.pop(0))
+        right = node('0x0','0x0',base.pop(0))
+        parent = node(left.getHash(),right.getHash(),'0x0')
+        left.adopt(parent)
+        left.adopt(parent)
         parents.append(parent)
     return parents
 
 class node:
-    def __init__(self, left, right):
+    def __init__(self, left, right,hash):
+        if (left == '0x0'):
+            self.hash = hash
+            return
+
         self.left = left
         self.right = right
         self.hash = hasher(left,right);
 
-    def adopt(parent):
+    def adopt(self,parent):
         self.parent = parent
     def getHash(self):
         return self.hash
-
+    def getParent(self):
+        return self.parent
+    def getRight(self):
+        return self.right
+    def getLeft(self):
+        return self.left
 
 currentDepth = []
 stem = []
@@ -63,3 +76,19 @@ with open('leaves') as fp:
         stem.append(currentDepth)
         print("Depth: " , len(stem))
     print("root: " , currentDepth[-1].getHash().hex())
+    print(i)
+    print(currentDepth)
+    print(len(stem))
+    leaf= stem[2][-1]
+    k = 0
+    while ((k-1) > j):
+        leaf = leaf.getParent()
+        k = k+1
+    parent = leaf.getParent()
+    if parent.getLeft() != leaf.getHash():
+        print('L', parent.getLeft().Hex()
+              )
+    else:
+        print('R',parent.getRight().hex())
+
+    print(leaf)

ea4c211617c66c85328a7654edfcb1737976949b
Filippa <af.delaval@gmail.com>
2017-11-22 13:26:26 +0000

    Solved H2 1

--- HA2/DCnets.py
+++ HA2/DCnets.py
@@ -1,26 +1,27 @@
 from binascii import unhexlify, hexlify
 from itertools import cycle
 
-SA = "27C2"
-SB = "0879"
+SA = "D75C"
+SB = "EE87"
 
-DA = "35F6"
-DB = "1A4D"
-M = "27BC"
+DA = "C568"
+DB = "FCB3"
+M = "4674"
 
 b = 1
 
 
 send=(bytes(c1 ^ c2 for c1, c2 in zip(unhexlify(SA), unhexlify(SB))).hex())
+
 if b==0:
 	A=(bytes(c1 ^ c2 for c1, c2 in zip(unhexlify(SA), unhexlify(DA))).hex())
 	B=(bytes(c1 ^ c2 for c1, c2 in zip(unhexlify(SB), unhexlify(DB))).hex())
 	recieve = bytes(c1 ^ c2 for c1, c2 in zip(unhexlify(A), unhexlify(B))).hex()
 	send+=recieve
+
 else:
 	invert = "FFFF"
-	M = (bytes(c1 ^ c2 for c1, c2 in zip(unhexlify(send), unhexlify(M))).hex())
-	M = (bytes(c1 ^ c2 for c1, c2 in zip(unhexlify(invert), unhexlify(M))).hex())
+	send = (bytes(c1 ^ c2 for c1, c2 in zip(unhexlify(send), unhexlify(M))).hex())
 
 print(send)
 

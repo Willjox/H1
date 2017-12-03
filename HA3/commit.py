eb9dd628fb2c34433bda6c85c11f81708d85f08e
willjox <William.jox@gmail.com>
2017-12-03 12:55:25 +0100

    Commitv2

---
+++ HA3/commit.py
@@ -0,0 +1,52 @@
+import random
+import hashlib
+import math
+
+
+
+def stddev(results, average):
+    y= []
+    for x in results:
+        y.append((x - average)**2)
+    return math.sqrt( ( (sum(y))/len(results)) )
+
+def iterate(confidence):
+
+    avg = 0
+    ci = 0
+    s = 0
+    currentconfidence = 10000
+    results = []
+    while confidence < currentconfidence:
+        results.append(binding())
+        avg = sum(results)/len(results)
+        if len(results) > 10:
+            s = stddev(results,avg)
+            currentconfidence = s/(math.sqrt(len(results)))
+            print("Confidence: ", currentconfidence)
+            print(avg)
+
+    return avg
+def binding():
+    rand = random.SystemRandom()
+    i = 0
+    l = 20
+    k = bytes(rand.getrandbits(16))
+    v = bytes(1)
+    x =  hashlib.sha1(v + k).digest()[:l]
+    v = bytes(2)
+    y = bytes(0)
+
+    while x != y:
+        k = bytes(rand.getrandbits(16))
+        y =  hashlib.sha1(v + k).digest()[:l]
+        i = i + 1
+        #print(i, " ",y.hex(),"\r" ,end='')
+
+    return(i)
+
+
+print(iterate(2000))
+
+
+#def concealing():

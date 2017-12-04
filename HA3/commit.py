d511ea0694a11910cd8f7d32b0bce0bce0776201
william <william.jox@gmail.com>
2017-12-04 23:06:21 +0100

    commit bitarray

--- HA3/commit.py
+++ HA3/commit.py
@@ -1,6 +1,7 @@
 import random
 import hashlib
 import math
+from bitarray import bitarray
 
 
 
@@ -31,20 +32,25 @@ def iterate(confidence):
 
     return avg
 def binding():
+    l = 2
     rand = random.SystemRandom()
+    k = (rand.getrandbits(16)).to_bytes(2, byteorder='big')
+    commit = bitarray()
+    commit.frombytes(k)
+    commit.append(True)
+    x = hashlib.sha1(commit.tobytes()).digest()[:l]
     i = 0
-    l = 20
-    k = bytes(random.getrandbits(16))
-    v = bytes(1)
-    x =  hashlib.sha1(v + k).digest()[:l]
-    v = bytes(2)
-    y = bytes(0)
-
+    y = 0
+    print(x.hex())
     while x != y:
-        k = bytes(rand.getrandbits(16))
-        y =  hashlib.sha1(v + k).digest()[:l]
+        k = (rand.getrandbits(16)).to_bytes(2, byteorder='big')
+        commit = bitarray()
+        commit.frombytes(k)
+        commit.append(False)
+        y = hashlib.sha1(commit.tobytes()).digest()[:l]
         i = i + 1
-        #print(i, " ",y.hex(),"\r" ,end='')
+        print(i, " ",y.hex(),"\r" ,end='')
+
     return(i)
 
 def concealing():
@@ -61,7 +67,6 @@ def concealing():
             k = bytes(rand.getrandbits(16))
             y =  hashlib.sha1(v + k).digest()[:l]
             i = i + 1
-            #print(i, " ",y.hex(),"\r" ,end='')
         return(i)
 
 print(iterate(500))

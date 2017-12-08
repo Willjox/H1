e6cf194872a24eceb01e752f720fb96af9b1963c
willjox <william.jox@gmail.com>
2017-12-08 15:16:12 +0100

    forge utan webbskit

--- HA4/forge.py
+++ HA4/forge.py
@@ -1,14 +1,33 @@
+import string
+import time
 avg = 999999999
 #signature = bytearray(10).tohex()
 #buildingblocks = list(10)
 best = 0
 cur = 0
+hexa = string.hexdigits[:16]
+sig = list("00000000000000000000")
 
-for i in range(0, 1):
-    for k in range (0, 256):
-            for z in range (0, 1):
-                byteblock = k.to_bytes(1,byteorder='big')
-                print (byteblock.hex())
-            #if bestTime < curTime:
-            #    bestTime = curTime
-            #    best = cur
+print(len(sig))
+print(hexa)
+x = hexa[0]
+print(x)
+input("Name: ")
+input("Grade: ")
+for i in range(0, 20):
+    testsig = list(sig)
+    for k in hexa:
+        testsig[i] = k
+        print("HAXXXING IN PROGRESS: ",testsig, "\r",end='')
+        for z in range(0,10):
+            start = time.clock()
+            #WEBJOX
+            stop = time.clock()
+            curTime = stop - start
+
+        if curTime > bestTime:
+                bestTime = curTime
+                best = k
+
+    sig[i] = best
+print("HAXXXING COMPLETE: ",sig, "\r",end='')

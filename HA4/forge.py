33996f6f298dd7bcacbe229e2ecba5d8befd2e8b
willjox <William.jox@gmail.com>
2017-12-09 11:51:47 +0100

    mer forge

--- HA4/forge.py
+++ HA4/forge.py
@@ -1,41 +1,45 @@
 import string
 import time
 import urllib.request
-avg = 999999999
-#signature = bytearray(10).tohex()
-#buildingblocks = list(10)
-best = 0
-cur = 0
+import ssl
+
+
 hexa = string.hexdigits[:16]
 sig = list("00000000000000000000")
 bestTime = 0
 def buildurl(name,sig,grade):
-    return "http://eitn41.eit.lth.se:3119/ha4/addgrade.php?name={}&grade={}&signature={}".format(name,grade,"".join(sig))
+    return "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name={}&grade={}&signature={}".format(name,grade,"".join(sig))
 
 print(len(sig))
 print(hexa)
 x = hexa[0]
 print(x)
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE #Python/min dator/manjaro gillar inte ert certifikat, kan ju lika gärna vara NSA som lyssnar nu!11!
+
 name = input("Name: ")
 grade = input("Grade: ")
 for i in range(0, 20):
     testsig = list(sig)
+
     for k in hexa:
         testsig[i] = k
+        curTime  = 0
         print("HAXXXING IN PROGRESS: ","".join(testsig), "\r",end='')
-        for z in range(0,10):
+        for z in range(0,5):
             start = time.clock()
-            http = buildurl(name,testsig,grade)
-            print (http)
-            with urllib.request.urlopen(http) as response:
+            with urllib.request.urlopen(buildurl(name,testsig,grade), context=ctx) as response:
                 stop = time.clock()
                 curTime = curTime + (stop-start)
-            print(curTime)
-            print("najs")
 
+
+        print(curTime)
         if curTime > bestTime:
                 bestTime = curTime
                 best = k
+                print(k)
+                print(bestTime)
 
     sig[i] = best
 print("HAXXXING COMPLETE: ","".join(sig))

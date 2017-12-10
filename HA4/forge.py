f3bafb0dc54aaca4bb5cf09cba2ac154dba5d6f2
willjox <william.jox@gmail.com>
2017-12-10 16:26:57 +0100

    bugsquash

--- HA4/forge.py
+++ HA4/forge.py
@@ -8,7 +8,7 @@ import ssl
 def buildurl(name,sige,grade):
     return "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name={}&grade={}&signature={}".format(name,grade,"".join(sige))
 def testSignature(url):
-    urllib.request.urlopen(url, context=ctx)
+    #urllib.request.urlopen(url, context=ctx)
     avgtime = 0
     testtime = 0
     start = time.time()
@@ -17,25 +17,18 @@ def testSignature(url):
     avgtime = stop-start
     return avgtime
 
-hexa = string.hexdigits[:16]
-sig = list("00000000000000000000")
-name = "Kalle"
-grade = "5"
-bestTime = 0
-testTime = 0
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE #Python/min dator/manjaro gillar inte ert certifikat, kan ju lika gärna vara NSA som lyssnar nu!11!
-#name = input("Name: ")
-#grade = input("Grade: ")
-
-
+hexa = string.hexdigits[:16]
+sig = list("00000000000000000000")
+name = input("Name: ")
+grade = input("Grade: ")
 
 for i in range(0, 20):
     testsig = list(sig)
-    #for k in range (0,255):
+    bestTime = 0
     for k in hexa:
-
         testsig[i] = k
         url = buildurl(name,testsig,grade)
         print("HAXXXING IN PROGRESS: ", "".join(testsig),"\r",end="")
@@ -43,6 +36,5 @@ for i in range(0, 20):
         if bestTime < testtime:
             bestTime = testtime
             sig = list(testsig)
-    bestTime = 0
-print("HAXXXING COMPLETE: ",join(testsig))
+print("HAXXXING COMPLETE:   ","".join(sig))
 print(buildurl(name,sig,grade))
